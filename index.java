7ec8db1e3756c183e94bf5a0e9a65808c0f023ea
amaan848 <amaaniqbal848@icloud.com>
2023-02-20 15:02:19 +0530

    added line x to feature2

--- index.java
+++ index.java
@@ -2,5 +2,6 @@ public class index{
     public static void main(String[] args) {
         System.out.println("Hello mister!!!!!!!!!");
         System.out.println("Hello mister!!!!!!!!!");
+        System.out.println("added line x");
     }
 }
